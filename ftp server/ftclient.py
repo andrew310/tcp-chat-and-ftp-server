9466d4b938963676c37f5837bdd25fa626cc1bd4
Andrew Brown <andrewbrown600@gmail.com>
2016-05-20 16:17:57 -0700

    everything working up to server responding with results of the ls command

--- ftp server/ftclient.py
+++ ftp server/ftclient.py
@@ -9,36 +9,67 @@ import os
 
 MSGLEN = 100
 
-def handler(sig, frame):
-    clientSocket.close()
-
 def sendCommand(socket, msg):
-    print "sending the goods"
+    print "sending the goods" + msg
     socket.send(msg)
 
+def getResponse(socket):
+    response = ""
+    try:
+        response = socket.recv(500)
+    except: pass
+    return response
+
 def main():
+
+    context = ""
     if(len(sys.argv) < 4):
         print "Usage: ./ftclient.py <hostname> <port number> <arg>"
         sys.exit(0)
+    elif(len(sys.argv) == 4):
+        #basically just building a string out of the argv just like how it would appear in c
+        context = " ".join([sys.argv[2], sys.argv[3]])
+    elif(len(sys.argv) == 5):
+        context = " ".join([sys.argv[2], sys.argv[3], sys.argv[4]])
 
-    for arg in sys.argv:
-        print arg
+    print len(sys.argv)
 
+    #server socket information
     hostName = sys.argv[1]
     hostPort = int(sys.argv[2]);
-    print hostPort
     #assign client socket, use my ip
     clientSocket = socket(AF_INET, SOCK_STREAM)
     #separate socket for receiving data
     dataSocket = socket(AF_INET, SOCK_STREAM)
-    context = sys.argv[3]
 
+    #close sockets safely when we ctrl+c
+    def handler(sig, frame):
+        clientSocket.close()
     #setup for signal interrupt to close socket
     signal.signal(signal.SIGINT, handler)
 
-    clientSocket.connect((hostName, hostPort))
+    if sys.argv[3] == -1:
+        print context + " hi "
+        #send on the dataSocket so it will be received here
+        clientSocket.connect((hostName, hostPort))
+        sendCommand(clientSocket, context)
 
-    sendCommand(clientSocket, context)
+        while 1:
+            res = clientSocket.recv(500)
+            if not msg:
+                break
+            print msg
+
+
+    else:
+        clientSocket.connect((hostName, hostPort))
+        sendCommand(clientSocket, context)
+        res = clientSocket.recv(500)
+
+        if res:
+            print res + '\n'
+            sys.exit(0)
+
 
 
 if __name__ == "__main__":
